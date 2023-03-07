d7b052790bd20aea56d45eb8967dcdabb3026d2b
dncuong <dinhcuongnd2001@gmail.com>
2023-03-07 07:47:32 +0700

    Feature/ApiKey & Permission

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 // *************************** init db ***************************
 
 require("./dbs/init.mongodb");
